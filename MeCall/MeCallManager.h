3cb2c4980784b0ac0c68e7a34b2e35ae35bf4511
Yep Mun Chun <munchun.yep@u2systems.com>
2016-08-03 17:16:04 +0800

    Complete basic feature

--- MeCall/MeCallManager.h
+++ MeCall/MeCallManager.h
@@ -10,6 +10,96 @@
 
 @interface MeCallManager : NSObject
 
-extern NSString *const MCIncomingCallNotification;
+extern NSString *const MCNotification_Registration;
+extern NSString *const MCNotification_Call;
+
+typedef enum _MCSipTransport {
+    MCSipTransport_UDP,
+    MCSipTransport_TCP,
+    MCSipTransport_TLS,
+    MCSipTransport_DTLS
+} MCSipTransport;
+
+typedef enum _MCMediaTransport {
+    MCMediaTransport_RTP,
+    MCMediaTransport_SRTP,
+    MCMediaTransport_ZRTP,
+    MCMediaTransport_DTLS
+} MCMediaTransport;
+
+typedef enum _MCRegistrationState {
+    MCRegistrationNone,
+    MCRegistrationProgress,
+    MCRegistrationOk,
+    MCRegistrationCleared,
+    MCRegistrationFailed
+} MCRegistrationState;
+
+typedef enum _MCCallState{
+    MCCallIdle,
+    MCCallIncomingReceived,
+    MCCallOutgoingInit,
+    MCCallOutgoingProgress,
+    MCCallOutgoingRinging,
+    MCCallOutgoingEarlyMedia,
+    MCCallConnected,
+    MCCallStreamsRunning,
+    MCCallPausing,
+    MCCallPaused,
+    MCCallResuming,
+    MCCallRefered,
+    MCCallError,
+    MCCallEnd,
+    MCCallPausedByRemote,
+    MCCallUpdatedByRemote,
+    MCCallIncomingEarlyMedia,
+    MCCallUpdating,
+    MCCallReleased,
+    MCCallEarlyUpdatedByRemote,
+    MCCallEarlyUpdating
+} MCCallState;
+
+typedef enum _MCReason{
+    MCReasonNone,
+    MCReasonNoResponse,
+    MCReasonForbidden,
+    MCReasonDeclined,
+    MCReasonNotFound,
+    MCReasonNotAnswered,
+    MCReasonBusy,
+    MCReasonUnsupportedContent,
+    MCReasonIOError,
+    MCReasonDoNotDisturb,
+    MCReasonUnauthorized,
+    MCReasonNotAcceptable,
+    MCReasonNoMatch,
+    MCReasonMovedPermanently,
+    MCReasonGone,
+    MCReasonTemporarilyUnavailable,
+    MCReasonAddressIncomplete,
+    MCReasonNotImplemented,
+    MCReasonBadGateway,
+    MCReasonServerTimeout,
+    MCReasonUnknown
+} MCReason;
+
++ (void)setupProxyConfig:(NSString*)sipUsername sipPassword:(NSString*)sipPassword sipServer:(NSString*)sipServer sipIdentity:(NSString*)sipIdentity;
++ (void)setupSipTransport:(MCSipTransport)sipTransport sipPort:(int)sipPort userAgentName:(NSString*)userAgentName userAgentVersion:(NSString*)userAgentVersion;
++ (void)setupMediaTransport:(MCMediaTransport)mediaTransport audioPort:(int)audioPort;
++ (void)setupAudioCodec:(NSArray*)audioCodecs;
+
++ (void)startRegistering:(int)expire;
++ (void)stopRegistering;
++ (MCRegistrationState)sipRegistrationState;
++ (MCReason)sipRegistrationFailReason;
+
++ (void)acceptCall:(NSString*)remote;
++ (void)declineCall:(NSString*)remote reason:(MCReason)reason;
++ (void)terminateCall:(NSString*)remote;
++ (void)inviteCall:(NSString*)remote;
++ (void)pauseCall:(NSString*)remote;
++ (void)resumeCall:(NSString*)remote;
+
++ (void)printAudioCodecSequence;
 
 @end
--- MeCall/MeCallManager.m
+++ MeCall/MeCallManager.m
@@ -11,9 +11,8 @@
 
 @implementation MeCallManager
 
-NSString *const MCIncomingCallNotification = @"MCIncomingCallNotification";
-
-static LinphoneCore* linphoneCore = nil;
+NSString *const MCNotification_Registration = @"MCNotification_Registration";
+NSString *const MCNotification_Call         = @"MCNotification_Call";
 
 extern void libmssilk_init(MSFactory *factory);
 extern void libmsamr_init(MSFactory *factory);
@@ -22,90 +21,10 @@ extern void libmsopenh264_init(MSFactory *factory);
 extern void libmsbcg729_init(MSFactory *factory);
 extern void libmswebrtc_init(MSFactory *factory);
 
-struct codec_name_pref_table {
-    const char *name;
-    int rate;
-    const char *prefname;
-};
-struct codec_name_pref_table codec_pref_table[] = {
-    {"speex", 8000, "speex_8k_preference"},
-    {"speex", 16000, "speex_16k_preference"},
-    {"silk", 24000, "silk_24k_preference"},
-    {"silk", 16000, "silk_16k_preference"},
-    {"amr", 8000, "amr_preference"},
-    {"gsm", 8000, "gsm_preference"},
-    {"ilbc", 8000, "ilbc_preference"},
-    {"isac", 16000, "isac_preference"},
-    {"pcmu", 8000, "pcmu_preference"},
-    {"pcma", 8000, "pcma_preference"},
-    {"g722", 8000, "g722_preference"},
-    {"g729", 8000, "g729_preference"},
-    {"mp4v-es", 90000, "mp4v-es_preference"},
-    {"h264", 90000, "h264_preference"},
-    {"vp8", 90000, "vp8_preference"},
-    {"mpeg4-generic", 16000, "aaceld_16k_preference"},
-    {"mpeg4-generic", 22050, "aaceld_22k_preference"},
-    {"mpeg4-generic", 32000, "aaceld_32k_preference"},
-    {"mpeg4-generic", 44100, "aaceld_44k_preference"},
-    {"mpeg4-generic", 48000, "aaceld_48k_preference"},
-    {"opus", 48000, "opus_preference"},
-    {NULL, 0, Nil}
-};
+static LinphoneCore* linphoneCore = nil;
 
-
-static void mecall_global_state_changed(LinphoneCore *lc, LinphoneGlobalState gstate, const char *message)
++ (void)initialize
 {
-    NSLog(@"mecall_global_state_changed: %s (message: %s)", linphone_global_state_to_string(gstate), message);
-}
-
-static void mecall_registration_state_changed(LinphoneCore *lc, LinphoneProxyConfig *cfg, LinphoneRegistrationState state, const char *message)
-{
-    NSLog(@"mecall_registration_state_changed: %s (message: %s)", linphone_registration_state_to_string(state), message);
-}
-
-static void mecall_call_state_changed(LinphoneCore *lc, LinphoneCall *call, LinphoneCallState state, const char *message)
-{
-    NSLog(@"mecall_call_state_changed: %s (message: %s)", linphone_call_state_to_string(state), message);
-}
-
-static void mecall_auth_info_requested(LinphoneCore *lc, const char *realmC, const char *usernameC, const char *domainC)
-{
-    NSLog(@"mecall_auth_info_requested");
-}
-
-static void mecall_configuring_status(LinphoneCore *lc, LinphoneConfiguringState status, const char *message)
-{
-    NSLog(@"mecall_configuring_status: %s (message: %s)", linphone_configuring_state_to_string(status), message);
-}
-
-static void mecall_notify_received(LinphoneCore *lc, LinphoneEvent *lev, const char *notified_event, const LinphoneContent *body)
-{
-    NSLog(@"mecall_notify_received: %s", notified_event);
-}
-
-static void mecall_call_encryption_changed(LinphoneCore *lc, LinphoneCall *call, bool_t on, const char *authentication_token)
-{
-    NSLog(@"mecall_call_encryption_changed: %@", on?@"On":@"Off");
-}
-
-static LinphoneCoreVTable linphonec_vtable = {
-    .global_state_changed = mecall_global_state_changed,
-    .registration_state_changed = mecall_registration_state_changed,
-    .call_state_changed = mecall_call_state_changed,
-    .auth_info_requested = mecall_auth_info_requested,
-    .configuring_status = mecall_configuring_status,
-    .notify_received = mecall_notify_received,
-    .call_encryption_changed = mecall_call_encryption_changed
-};
-
-
-
-+ (void)setup
-{
-    if (linphoneCore != nil) {
-        return;
-    }
-    
     NSString *defaultConfig = [[NSBundle mainBundle] pathForResource:@"linphonerc" ofType:nil];
     NSString *factoryConfig = [[NSBundle mainBundle] pathForResource:@"linphonerc-factory" ofType:nil];
     linphoneCore = linphone_core_new(&linphonec_vtable, [defaultConfig UTF8String], [factoryConfig UTF8String], nil);
@@ -119,28 +38,218 @@ static LinphoneCoreVTable linphonec_vtable = {
     libmswebrtc_init(f);
     linphone_core_reload_ms_plugins(linphoneCore, NULL);
     
+    linphone_core_set_ring(linphoneCore, [[[NSBundle mainBundle] pathForResource:@"notes_of_the_optimistic" ofType:@"caf"] UTF8String]);
+    linphone_core_set_ringback(linphoneCore, [[[NSBundle mainBundle] pathForResource:@"ringback" ofType:@"wav"] UTF8String]);
+    linphone_core_set_play_file(linphoneCore, [[[NSBundle mainBundle] pathForResource:@"hold" ofType:@"mkv"] UTF8String]);
     
+    [MeCallManager setupAudioCodec:@[@"g722",@"pcma"]];
+    [NSTimer scheduledTimerWithTimeInterval:0.02 target:self selector:@selector(iterate) userInfo:nil repeats:YES];
 }
 
++ (void)iterate {
+    linphone_core_iterate(linphoneCore);
+}
 
-+ (void)setAudioCodecHighPriority:(const char*)type rate:(int)rate channels:(int)channels
++ (void)setupProxyConfig:(NSString*)sipUsername sipPassword:(NSString*)sipPassword sipServer:(NSString*)sipServer sipIdentity:(NSString*)sipIdentity
 {
-    PayloadType *pt=linphone_core_find_payload_type(linphoneCore, type, rate, channels);
-    const bctbx_list_t *list = linphone_core_get_audio_codecs(linphoneCore);
-    bctbx_list_t *head = list->next->prev;
-    bctbx_list_t *elem = bctbx_list_find(head, pt);
+    linphone_core_clear_proxy_config(linphoneCore);
+    linphone_core_clear_all_auth_info(linphoneCore);
+    LinphoneProxyConfig* proxyCfg = linphone_core_create_proxy_config(linphoneCore);
+    LinphoneAddress *identity = linphone_address_new([sipIdentity UTF8String]);
+    linphone_proxy_config_set_identity_address(proxyCfg, identity);
+    linphone_proxy_config_set_server_addr(proxyCfg, [sipServer UTF8String]);
+    linphone_proxy_config_set_route(proxyCfg, [sipServer UTF8String]);
+    linphone_proxy_config_set_expires(proxyCfg, 0);
+    linphone_proxy_config_enable_register(proxyCfg, false);
+    LinphoneAuthInfo* authInfo = linphone_auth_info_new([sipUsername UTF8String], NULL, [sipPassword UTF8String], NULL, NULL, linphone_proxy_config_get_domain(proxyCfg));
+    linphone_core_add_auth_info(linphoneCore, authInfo);
+    linphone_core_add_proxy_config(linphoneCore, proxyCfg);
+    linphone_core_set_default_proxy_config(linphoneCore, proxyCfg);
+    linphone_address_destroy(identity);
+    linphone_auth_info_destroy(authInfo);
+}
+
++ (void)setupSipTransport:(MCSipTransport)sipTransport sipPort:(int)sipPort userAgentName:(NSString*)userAgentName userAgentVersion:(NSString*)userAgentVersion
+{
+    LCSipTransports sipTransports = {0,0,0,0};
+    sipTransports.udp_port  = (sipTransport == MCSipTransport_UDP)  ? sipPort : 0;
+    sipTransports.tcp_port  = (sipTransport == MCSipTransport_TCP)  ? sipPort : 0;
+    sipTransports.tls_port  = (sipTransport == MCSipTransport_TLS)  ? sipPort : 0;
+    sipTransports.dtls_port = (sipTransport == MCSipTransport_DTLS) ? sipPort : 0;
+    linphone_core_set_sip_transports(linphoneCore, &sipTransports);
+    linphone_core_set_user_agent(linphoneCore, [userAgentName UTF8String], [userAgentVersion UTF8String]);
+}
+
++ (void)setupMediaTransport:(MCMediaTransport)mediaTransport audioPort:(int)audioPort
+{
+    LinphoneMediaEncryption encryption = LinphoneMediaEncryptionNone;
+    if (mediaTransport == MCMediaTransport_SRTP) encryption = LinphoneMediaEncryptionSRTP;
+    if (mediaTransport == MCMediaTransport_ZRTP) encryption = LinphoneMediaEncryptionZRTP;
+    if (mediaTransport == MCMediaTransport_DTLS) encryption = LinphoneMediaEncryptionDTLS;
+    linphone_core_set_media_encryption(linphoneCore, encryption);
+    linphone_core_set_audio_port(linphoneCore, audioPort);
+}
+
++ (void)setupAudioCodec:(NSArray*)audioCodecs
+{
+    for (const bctbx_list_t *elem=linphone_core_get_audio_codecs(linphoneCore); elem!=NULL; elem=elem->next)
+        linphone_core_enable_payload_type(linphoneCore, elem->data, FALSE);
     
-    if(elem && elem!=head)
+    bctbx_list_t *head = bctbx_list_copy(linphone_core_get_audio_codecs(linphoneCore));
+    for (NSString *codec in [audioCodecs reverseObjectEnumerator])
     {
-        if(elem->next)
-            elem->next->prev = elem->prev;
-        
-        elem->prev->next = elem->next;
-        elem->prev = NULL;
-        elem->next = head;
-        linphone_core_set_audio_codecs(linphoneCore, bctbx_list_copy(elem));
+        PayloadType *pt = linphone_core_find_payload_type(linphoneCore, [codec UTF8String], LINPHONE_FIND_PAYLOAD_IGNORE_RATE, LINPHONE_FIND_PAYLOAD_IGNORE_CHANNELS);
+        if (pt != NULL)
+        {
+            linphone_core_enable_payload_type(linphoneCore, pt, TRUE);
+            head = bctbx_list_remove(head, pt);
+            head = bctbx_list_prepend(head, pt);
+        }
+    }
+    linphone_core_set_audio_codecs(linphoneCore, head);
+}
+
++ (void)startRegistering:(int)expire
+{
+    LinphoneProxyConfig* proxyCfg = linphone_core_get_default_proxy_config(linphoneCore);
+    if (proxyCfg == NULL) return;
+    linphone_proxy_config_edit(proxyCfg);
+    linphone_proxy_config_set_expires(proxyCfg, expire);
+    linphone_proxy_config_enable_register(proxyCfg, true);
+    linphone_proxy_config_done(proxyCfg);
+}
+
++ (void)stopRegistering
+{
+    LinphoneProxyConfig* proxyCfg = linphone_core_get_default_proxy_config(linphoneCore);
+    if (proxyCfg == NULL) return;
+    linphone_proxy_config_edit(proxyCfg);
+    linphone_proxy_config_set_expires(proxyCfg, 0);
+    linphone_proxy_config_enable_register(proxyCfg, false);
+    linphone_proxy_config_done(proxyCfg);
+}
+
++ (MCRegistrationState)sipRegistrationState
+{
+    LinphoneProxyConfig* proxyCfg = linphone_core_get_default_proxy_config(linphoneCore);
+    LinphoneRegistrationState state = linphone_proxy_config_get_state(proxyCfg);
+    return (MCRegistrationState)state;
+}
+
++ (MCReason)sipRegistrationFailReason
+{
+    LinphoneProxyConfig* proxyCfg = linphone_core_get_default_proxy_config(linphoneCore);
+    LinphoneReason reason = linphone_proxy_config_get_error(proxyCfg);
+    return (MCReason)reason;
+}
+
++ (void)acceptCall:(NSString*)remote
+{
+    LinphoneCall *call = [self getCallWithRemote:remote];
+    if (call != NULL) {
+        linphone_core_accept_call(linphoneCore, call);
     }
 }
+
++ (void)declineCall:(NSString*)remote reason:(MCReason)reason
+{
+    LinphoneCall *call = [self getCallWithRemote:remote];
+    if (call != NULL) {
+        linphone_core_decline_call(linphoneCore, call, (LinphoneReason)reason);
+    }
+}
+
++ (void)terminateCall:(NSString*)remote
+{
+    LinphoneCall *call = [self getCallWithRemote:remote];
+    if (call != NULL) {
+        linphone_core_terminate_call(linphoneCore, call);
+    }
+}
+
++ (void)inviteCall:(NSString*)remote
+{
+    LinphoneAddress *address = linphone_core_interpret_url(linphoneCore, [remote UTF8String]);
+    if (address != NULL) {
+        linphone_core_invite_address(linphoneCore, address);
+    }
+}
+
++ (void)pauseCall:(NSString*)remote
+{
+    LinphoneCall *call = [self getCallWithRemote:remote];
+    if (call != NULL) {
+        linphone_core_pause_call(linphoneCore, call);
+    }
+}
+
++ (void)resumeCall:(NSString*)remote
+{
+    LinphoneCall *call = [self getCallWithRemote:remote];
+    if (call != NULL) {
+        linphone_core_resume_call(linphoneCore, call);
+    }
+}
+
++ (LinphoneCall*)getCallWithRemote:(NSString*)remote
+{
+    const bctbx_list_t *calls = linphone_core_get_calls(linphoneCore);
+    while (calls != NULL)
+    {
+        LinphoneCall *call = (LinphoneCall*)calls->data;
+        const LinphoneAddress* address = linphone_call_get_remote_address(call);
+        const char* username = linphone_address_get_username(address);
+        if([remote isEqualToString:[NSString stringWithUTF8String:username]]) {
+            return call;
+        }
+        calls = calls->next;
+    }
+    return NULL;
+}
+
+
+static void mecall_registration_state_changed(LinphoneCore *lc, LinphoneProxyConfig *cfg, LinphoneRegistrationState state, const char *message)
+{
+    NSLog(@"mecall_registration_state_changed: %s (message: %s)", linphone_registration_state_to_string(state), message);
+    LinphoneReason reason = linphone_proxy_config_get_error(cfg);
+    NSDictionary *dict = @{@"state":[NSNumber numberWithInt:state],
+                           @"failReason":[NSNumber numberWithInt:reason]};
+    [[NSNotificationCenter defaultCenter] postNotificationName:MCNotification_Registration object:nil userInfo:dict];
+}
+static void mecall_call_state_changed(LinphoneCore *lc, LinphoneCall *call, LinphoneCallState state, const char *message)
+{
+    NSLog(@"mecall_call_state_changed: %s (message: %s)", linphone_call_state_to_string(state), message);
+    const LinphoneAddress *address = linphone_call_get_remote_address(call);
+    const char* username = linphone_address_get_username(address);
+    NSDictionary *dict = @{@"state":[NSNumber numberWithInt:state],
+                           @"remote":[NSString stringWithUTF8String:username]};
+    [[NSNotificationCenter defaultCenter] postNotificationName:MCNotification_Call object:nil userInfo:dict];
+}
+static void mecall_global_state_changed(LinphoneCore *lc, LinphoneGlobalState gstate, const char *message) {
+    NSLog(@"mecall_global_state_changed: %s (message: %s)", linphone_global_state_to_string(gstate), message);
+}
+static void mecall_auth_info_requested(LinphoneCore *lc, const char *realmC, const char *usernameC, const char *domainC) {
+    NSLog(@"mecall_auth_info_requested");
+}
+static void mecall_configuring_status(LinphoneCore *lc, LinphoneConfiguringState status, const char *message) {
+    NSLog(@"mecall_configuring_status: %s (message: %s)", linphone_configuring_state_to_string(status), message);
+}
+static void mecall_notify_received(LinphoneCore *lc, LinphoneEvent *lev, const char *notified_event, const LinphoneContent *body) {
+    NSLog(@"mecall_notify_received: %s", notified_event);
+}
+static void mecall_call_encryption_changed(LinphoneCore *lc, LinphoneCall *call, bool_t on, const char *authentication_token) {
+    NSLog(@"mecall_call_encryption_changed: %@", on?@"On":@"Off");
+}
+
+static LinphoneCoreVTable linphonec_vtable = {
+    .global_state_changed = mecall_global_state_changed,
+    .registration_state_changed = mecall_registration_state_changed,
+    .call_state_changed = mecall_call_state_changed,
+    .auth_info_requested = mecall_auth_info_requested,
+    .configuring_status = mecall_configuring_status,
+    .notify_received = mecall_notify_received,
+    .call_encryption_changed = mecall_call_encryption_changed
+};
 
 + (void)printAudioCodecSequence
 {
@@ -156,25 +265,5 @@ static LinphoneCoreVTable linphonec_vtable = {
               linphone_core_payload_type_enabled(linphoneCore, pt)?@"Yes":@"No");
     }
 }
-
-+ (void)ResetAudioCodec
-{
-    [self setAudioCodecHighPriority:"g729" rate:8000 channels:-1];
-    [self setAudioCodecHighPriority:"g722" rate:8000 channels:-1];
-    
-    int i=0;
-    for (const bctbx_list_t *elem=linphone_core_get_audio_codecs(linphoneCore); elem!=NULL; elem=elem->next)
-    {
-        linphone_core_enable_payload_type(linphoneCore, elem->data, (i<2));
-        i++;
-    }
-    
-    [self printAudioCodecSequence];
-}
-
-
-
-
-
 
 @end
